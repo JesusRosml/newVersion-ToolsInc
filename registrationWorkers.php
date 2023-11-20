3e854be2cb52431b0bf044d3b9b6959f9dbb5cc2
JesusRosml <jesusperezhidalgo16@outlook.com>
2023-11-19 18:31:49 -0600

    stockists registration section completed

--- registrationWorkers.php
+++ registrationWorkers.php
@@ -16,6 +16,7 @@
     <link rel="stylesheet" href="css/externalStyles/normalize.css">
     <link rel="stylesheet" href="css/navegationApp.css">
     <link rel="stylesheet" href="css/registrationTools/registrationTools.css">
+    <link rel="stylesheet" href="css/registrationTools/registrationWarehouseman.css">
 </head>
 <body>
     <?php
@@ -57,8 +58,8 @@
 
                 <select name="stateWorker">
                     <option value="">Estado actual</option>
-                    <option value="">Activo</option>
-                    <option value="">De baja</option>
+                    <option value="Activo">Activo</option>
+                    <option value="De baja">De baja</option>
                 </select>
 
                 <button type="submit">Registrar trabajador</button>
@@ -73,6 +74,38 @@
 
         <section class="container-button">
             <button id="registerStorer">Registrar almacenista</button>
+        </section>
+
+        <section class="container-warehouseman">
+            <div class="container-title">
+                <h1>Registro de almacenista</h1>
+                <button id="closeWarehouseman">Cerrar</button>
+            </div>
+
+            <form action="" method="POST">
+                <label for="nameWarehouseman">Ingrese el nombre completo</label>
+                <input name="nameWarehouseman" type="text" placeholder="Nombre del almacenista" id="nameWarehouseman">
+
+                <label for="phoneWarehouseman">Ingrese el telefóno</label>
+                <input name="phoneWarehouseman" type="text" placeholder="Numero telefónico" id="phoneWarehouseman">
+
+                <label for="emailWarehouseman">Ingrese el correo electronico</label>
+                <input name="emailWarehouseman" type="email" placeholder="Correo electronico" id="emailWarehouseman">
+
+                <label for="passwordFirstWarehouseman">Ingrese la contraseña</label>
+                <input name="passwordWarehouseman" type="password" placeholder="Contraseña" id="passwordFirstWarehouseman">
+
+                <label for="passwordSecondWarehouseman">Confirme la contraseña</label>
+                <input name="passwordSecondWarehouseman" type="password" placeholder="Contraseña" id="passwordSecondWarehouseman">
+
+                <select name="stateWharehouseman">
+                    <option value="">Estado actual</option>
+                    <option value="Activo">Activo</option>
+                    <option value="De baja">De baja</option>
+                </select>
+
+                <button type="submit">Registrar almacenista</button>
+            </form>
         </section>
     </main>
 
